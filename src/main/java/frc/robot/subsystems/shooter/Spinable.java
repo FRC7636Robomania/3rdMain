70de4fa9cdc6f3340368b320832901b2fb19efd6
Yu_Yue <58898944+SJZ99@users.noreply.github.com>
2020-09-12 07:49:24 +0800

    Remove 註解

--- src/main/java/frc/robot/subsystems/shooter/Spinable.java
+++ src/main/java/frc/robot/subsystems/shooter/Spinable.java
@@ -18,10 +18,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * 這個類別可以被用來實現幾乎所有機器人上的馬達，每個馬達不同的地方可以在各自的類別中覆寫以下三個方法，
  * 搭配SpinForward和SpinReverse可以有效地減少命令的類別數量
  */
-public abstract class Spinable extends SubsystemBase {
-  /**
-   * Forward method. This will be override when extends.
-   */
+public abstract class Spinable extends SubsystemBase{
   public abstract void forward();
   public abstract void stop();
   public abstract void reverse();
